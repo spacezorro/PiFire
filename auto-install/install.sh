a6c532facd07a887ec9d1cd6e26342c18b508bce
spacezorro <spacezorro@gmail.com>
2025-10-29 11:19:40 -0700

    fix: in headless USER is not set

--- auto-install/install.sh
+++ auto-install/install.sh
@@ -269,6 +269,7 @@ echo "**                                                                     **"
 echo "*************************************************************************" | tee -a ~/logs/pifire_install.log
 echo ""
 echo " + Setting Up PiFire Group"
+USER=${USER:-pi}
 cd /usr/local/bin
 $SUDO groupadd pifire 
 $SUDO usermod -a -G pifire $USER 
